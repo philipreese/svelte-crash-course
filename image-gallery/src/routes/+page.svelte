370f5919d6adb241e0d2615e85d0987fc7fc2c5f
Philip Reese <philip.reese@gtri.gatech.edu>
2024-02-27 15:32:31 -0500

    Set up HTML and CSS for image gallery

--- image-gallery/src/routes/+page.svelte
+++ image-gallery/src/routes/+page.svelte
@@ -1,2 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+    <div class="header">
+        <h1>Image Gallery</h1>
+        <div class="input-container">
+            <input type="text" class="input" />
+            <button class="button">Search</button>
+        </div>
+    </div>
+    <div class="photos">
+        <img src="" alt="" class="image" />
+        <img src="" alt="" class="image" />
+        <img src="" alt="" class="image" />
+        <img src="" alt="" class="image" />
+    </div>
+</div>
+
+<style>
+    .image {
+        width: 400px;
+        height: 250px;
+        margin: 5px;
+    }
+    .photos {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .container {
+        width: 1230px;
+        margin: 0 auto;
+    }
+    .header {
+        text-align: center;
+        font-size: 20px;
+    }
+    .input {
+        padding: 10px;
+        width: 400px;
+        border-radius: 10px;
+        outline: none;
+        border: 1px solid gray;
+        font-size: 20px;
+    }
+    .button {
+        padding: 10px;
+        font-size: 20px;
+        background-color: aqua;
+        border-radius: 10px;
+        border: none;
+        color: white;
+    }
+    .input-container {
+        margin-bottom: 40px;
+    }
+</style>
