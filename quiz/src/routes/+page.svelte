dd4e8217267c8f850186fb7d2f32d5533a9ca3b5
Philip Reese <philip.reese@gtri.gatech.edu>
2024-02-28 12:44:40 -0500

    Add consistent layout across pages for quiz app

--- quiz/src/routes/+page.svelte
+++ quiz/src/routes/+page.svelte
@@ -3,10 +3,6 @@
     import Card from "../components/Card.svelte";
 </script>
 
-<div
-    class="quizzes-container w-[700px] m-auto flex justify-center h-screen items-center"
->
-    {#each quizzes as quiz (quiz.id)}
-        <Card {quiz} />
-    {/each}
-</div>
+{#each quizzes as quiz (quiz.id)}
+    <Card {quiz} />
+{/each}
