5083f382541403e33a15f40d9b77c9c945c1786d
Philip Reese <philip.reese@gtri.gatech.edu>
2024-02-29 11:56:45 -0500

    Add QuestionOption and QuestionText components to quiz app

--- quiz/src/routes/quiz/[id]/+page.svelte
+++ quiz/src/routes/quiz/[id]/+page.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
+    import QuestionOption from "./components/QuestionOption.svelte";
+    import QuestionText from "./components/QuestionText.svelte";
+
     export let data: any;
+
+    let currentQuestionIndex = 0;
+    $: question = data.questions[currentQuestionIndex];
 </script>
 
-<div>{data.name}</div>
+<div class="w-full">
+    <QuestionText text={question.question} />
+    <div class="flex justify-between flex-wrap cursor-pointer">
+        {#each question.options as option (option.id)}
+            <QuestionOption {option} />
+        {/each}
+    </div>
+</div>
